66dc7914e93e78a8f87411f559df212293b3022b
djimi <djimi9530@gmail.com>
2025-06-04 09:13:33 +0200

    convert columns containing string to boolean

--- transform.py
+++ transform.py
@@ -7,5 +7,15 @@ def transform():
     df = get_velib_data()
     # convert column names to snake case
     df.columns = df.columns.str.lower().str.replace(" ", "_")
+
+    # convert "station_en_fonctionnement" to boolean
+    names = {
+        "OUI": True,
+        "NON": False
+    }
+    df["station_en_fonctionnement"] = df["station_en_fonctionnement"].map(names)
+    df["retour_vélib_possible"] = df["retour_vélib_possible"].map(names)
+    df["borne_de_paiement_disponible"] = df["borne_de_paiement_disponible"].map(names)
+
     return df
 
